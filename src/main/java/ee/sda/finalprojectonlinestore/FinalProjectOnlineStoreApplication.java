685af3612078c61b22f1c5b344ef3fa37e9c17a7
Artur Krivorokov <artur.krivorokov@gmail.com>
2020-11-29 10:36:27 +0200

    initial commit

---
+++ src/main/java/ee/sda/finalprojectonlinestore/FinalProjectOnlineStoreApplication.java
@@ -0,0 +1,13 @@
+package ee.sda.finalprojectonlinestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProjectOnlineStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalProjectOnlineStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ee/sda/finalprojectonlinestore/FinalProjectOnlineStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.sda.finalprojectonlinestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectOnlineStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
